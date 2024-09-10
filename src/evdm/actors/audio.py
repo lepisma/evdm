dd95c9dbf728eec81ac48d687cc1951db108de98
Abhinav Tushar <abhinav@lepisma.xyz>
2024-09-10 15:34:04 +0530

    Refactor actors to have separate packages

--- src/evdm/actors/audio.py
+++ src/evdm/actors/audio.py
@@ -1,39 +1,10 @@
 """In built actors with focus on Spoken Dialog Systems."""
 
-from abc import abstractmethod, ABC
 from evdm.bus import BusType
-from evdm.events import Event, make_event
+from evdm.events import make_event
+from evdm.actors.core import Actor
 import sounddevice as sd
 import asyncio
-
-from loguru import logger
-
-
-class Actor(ABC):
-    """Abstract Actor class.
-
-    An actor subscribes for events from a bus and does some processing,
-    possibly resulting in emitting events to other buses.
-    """
-
-    @abstractmethod
-    async def act(self, event: Event, heb: "HEB"):
-        """Take `event` and do something with it.
-
-        After the compute is finished, optionally use heb to emit more messages.
-        """
-        raise NotImplementedError()
-
-
-class DebugTap(Actor):
-    """Actor that reads events on a bus and logs events at DEBUG level."""
-
-    def __init__(self, bus: BusType) -> None:
-        super().__init__()
-        self.bus = bus
-
-    async def act(self, event, heb):
-        logger.debug(f"{event} on {self.bus}")
 
 
 class MicrophoneListener(Actor):
