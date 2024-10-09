069b69314b03b56a274f079e4d81414e3a94a2a5
Abhinav Tushar <abhinav@lepisma.xyz>
2024-10-09 21:24:08 +0530

    OpenAI realtime speech to speech actor

--- src/evdm/actors/conversation.py
+++ src/evdm/actors/conversation.py
@@ -1,9 +1,213 @@
 """Actors related to conversation management."""
 
+import asyncio
+
 from evdm.actors.core import Actor
 from evdm.bus import BusType
 from evdm.events import Event, make_event
 import ollama
+import os
+import json
+from websockets.asyncio.client import connect
+from loguru import logger
+import soundfile as sf
+import io
+import numpy as np
+import base64
+
+
+class OpenAIRealtimeAgent(Actor):
+    """Agent that uses OpenAI Realtime API for two-party conversations.
+
+    This listens on Audio Signals bus (since we use server VAD mode from the
+    API) and emits to Audio Signals (output audio) and Lexical Segments
+    (transcripts) bus.
+
+    """
+
+    def __init__(self, prompt: str) -> None:
+        super().__init__()
+        self.prompt = prompt
+
+        # Reference to the heb for emitting events. This is set at the first
+        # call to `act`.
+        self.heb = None
+
+    async def connect(self):
+        self.ws = await self._connect()
+        await self._set_prompt()
+
+    async def start_loop(self):
+        await self._handle_server_events()
+
+    async def _connect(self):
+        url = "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01"
+        api_key = os.getenv("OPENAI_API_KEY")
+
+        if api_key is None:
+            raise ValueError("OPENAI_API_KEY not set.")
+
+        return await connect(url, additional_headers={
+            "Authorization": "Bearer " + api_key,
+            "OpenAI-Beta": "realtime=v1"
+        })
+
+    async def _set_prompt(self):
+        await self.ws.send(json.dumps({
+            "type": "session.update",
+            "session": {
+                "modalities": ["text", "audio"],
+                "instructions": self.prompt,
+                "voice": "alloy",
+                "input_audio_format": "pcm16",
+                "output_audio_format": "pcm16",
+                "input_audio_transcription": {
+                    "model": "whisper-1"
+                },
+                "turn_detection": {
+                    "type": "server_vad",
+                    "threshold": 0.5,
+                    "prefix_padding_ms": 300,
+                    "silence_duration_ms": 200
+                },
+                "tools": [],
+                "tool_choice": "none",
+                "temperature": 0.8
+            }
+        }))
+
+    async def _handle_server_events(self):
+        """Loop and listen to server events and then act accordingly. We ignore
+        most of the events for now but we still 'handle' all to ensure that any
+        upstream change doesn't blow up in our face.
+        """
+
+        async for message in self.ws:
+            data = json.loads(message)
+
+            match data["type"]:
+                case "error":
+                    await self.handle_error(data)
+                case "session.created":
+                    logger.debug("Session created.")
+                case "session.updated":
+                    pass
+                case "conversation.created":
+                    pass
+                case "input_audio_buffer.committed":
+                    pass
+                case "input_audio_buffer.cleared":
+                    pass
+                case "input_audio_buffer.speech_started":
+                    logger.debug("Input speech started")
+                case "input_audio_buffer.speech_stopped":
+                    logger.debug("Input speech stopped")
+                case "conversation.item.created":
+                    pass
+                case "conversation.item.input_audio_transcription.completed":
+                    logger.debug(f"Input speech transcript: {data['transcript']}")
+                case "conversation.item.input_audio_transcription.failed":
+                    pass
+                case "conversation.item.truncated":
+                    pass
+                case "conversation.item.deleted":
+                    pass
+                case "response.created":
+                    pass
+                case "response.done":
+                    pass
+                case "response.output_item.added":
+                    pass
+                case "response.output_item.done":
+                    pass
+                case "response.content_part.added":
+                    pass
+                case "response.content_part.done":
+                    pass
+                case "response.text.delta":
+                    pass
+                case "response.text.done":
+                    pass
+                case "response.audio_transcript.delta":
+                    pass
+                case "response.audio_transcript.done":
+                    logger.debug(f"Output text: {data['transcript']}")
+                case "response.audio.delta":
+                    await self.handle_audio_delta(data["delta"])
+                case "response.audio.done":
+                    pass
+                case "response.function_call_arguments.delta":
+                    pass
+                case "response.function_call_arguments.done":
+                    pass
+                case "rate_limits.updated":
+                    pass
+
+    async def handle_error(self, message_data: dict):
+        raise RuntimeError(f"Server error: {message_data}")
+
+    async def handle_audio_delta(self, encoded_audio: str):
+        samples, sr = await self.decode_audio(encoded_audio)
+
+        await self.heb.put(make_event({
+            "source": "bot:oai-realtime", "samples": samples, "sr": sr
+        }), BusType.AudioSignals)
+
+    async def encode_audio(self, audio_samples, samplerate: int) -> str:
+        """Encode audio to base64 encoded binary format that's acceptable via
+        the API.
+        """
+
+        # HACK since they are allowing only this sampling rate right now
+        assert samplerate == 24_000
+
+        def _write(buffer, samples, sr: int):
+            with sf.SoundFile(buffer, "w", format="RAW", samplerate=sr, channels=1, subtype="PCM_16", endian="LITTLE") as fp:
+                fp.write(samples)
+
+        with io.BytesIO() as buffer:
+            loop = asyncio.get_event_loop()
+            await loop.run_in_executor(None, lambda: _write(buffer, audio_samples, samplerate))
+
+            buffer.seek(0)
+            return base64.b64encode(buffer.read()).decode("utf-8")
+
+    async def decode_audio(self, data: str):
+        """Decode base64 audio `data` that's in raw PCM_16 little endian format
+        to samples and sampling rate."""
+
+        # This is the default for the API
+        sr = 24_000
+
+        binary_data = base64.b64decode(data)
+
+        def _read(buffer):
+            with sf.SoundFile(buffer, "r", format="RAW", samplerate=sr, channels=1, subtype="PCM_16", endian="LITTLE") as fp:
+                return fp.read()
+
+        with io.BytesIO(binary_data) as buffer:
+            loop = asyncio.get_event_loop()
+            samples = await loop.run_in_executor(None, lambda: _read(buffer))
+
+        samples = samples.astype(np.float32) / 32767
+        return samples.reshape(len(samples), 1) , sr
+
+    async def act(self, event: Event, heb):
+        if event.data["source"].startswith("bot:"):
+            return
+
+        if self.heb is None:
+            self.heb = heb
+
+        sr = event.data["sr"]
+        samples = event.data["samples"]
+
+        encoded_audio = await self.encode_audio(samples, sr)
+
+        await self.ws.send(json.dumps({
+            "type": "input_audio_buffer.append",
+            "audio": encoded_audio
+        }))
 
 
 class BackchannelDetectorAgent(Actor):
